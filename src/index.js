6260fb2625f3017f45c93ebcfc67c1a26855f5be
AA <aa@AAs-MacBook-Air.local>
2022-01-20 19:10:36 +0300

    feat: all task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result='';
+    n= Math.abs(n);
+  for (var i=0; i<n.toString().length; ++i)
+  {
+      let a= Math.floor(n/(10**i)) %10;
+      result=result.toString()+a.toString();
+  }
+  return Number(result);
 }
